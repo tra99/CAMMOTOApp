3c406e3b780c3a0137edabf5a4be8e87a487177a
HOK Sochetra <chetrafc60@gmail.com>
2023-07-31 00:08:34 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
-import 'package:cammoto_project/splashPage/splashpage.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
+import 'introduction_screen/intro1.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const SplashPage(),
+      home:  const InroPage(),
       debugShowCheckedModeBanner: false,
     );
   }
